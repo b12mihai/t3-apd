0332cdbd17818c68e0e5b7d4b0bbf618c7036fdc
b12mihai <b12mihai@gmail.com>
2012-12-13 22:52:11 +0200

    Merge versiunea MPI pentru Mandelbrot momentan, testele publice au fost efectuate

--- main.c
+++ main.c
@@ -8,74 +8,96 @@
 #include "complex.h"
 #include "util.h"
 
-int master(int num_tasks, Input in, char* file_out)
+/* Constants for message-passing */
+#define START_TAG   1           /* "start" message (master to worker) */
+#define DATA_TAG    2           /* "data" message (worker to master) */
+#define START_LNG   2
+
+
+int master(int nworkers, int width, int height, Input in, char* file_out)
 {
-	int width, height, this_row, row, col;
-	int **colors;
 	MPI_Status status;
-	int *data_msg;
+	long *data_msg, start_msg[START_LNG];
+	int first_row, this_row, rows, rows_per_worker_quot, rows_per_worker_rem;
+	int p;
 	
-	width = floor((in.x_max - in.x_min) / in.rezolutie);
-	height = floor((in.y_max - in.y_min) / in.rezolutie);
+	data_msg = malloc((width + 1) * sizeof(long));
 	
-	data_msg = malloc((width + 1) * sizeof(int));
-		
-	colors = malloc(height * sizeof(int*));
+	/* Calculate which rows each worker should work on. */
+    first_row = 0;
+    rows_per_worker_quot = height / nworkers;
+    rows_per_worker_rem = height % nworkers;
+    for (p = 0; p < nworkers; ++p) {
+        rows = rows_per_worker_quot;
+        if (p < rows_per_worker_rem)
+            ++rows;
+        start_msg[0] = first_row;
+        start_msg[1] = rows;
+        MPI_Send(start_msg, START_LNG, MPI_LONG, p+1,
+                START_TAG, MPI_COMM_WORLD);
+        first_row += rows;
+    }
+    
+    /* Receive results from workers and draw points */
+    int row, col;
+    int **colors;
+   	colors = malloc(height * sizeof(int*));
 	for(row = 0; row < height; ++row)
 		colors[row] = malloc(width * sizeof(int));
-		
-	for(row = 0; row < height; ++row) {
-		
-		MPI_Recv(data_msg, width + 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-		
-		this_row = data_msg[0];
-	
-		for(col = 0; col < width; ++col) {
-			colors[this_row][col] = data_msg[col + 1];
-		}
-	}
-	
-	plot_pgm(file_out, width, height, colors);
+    
+    for (row = 0; row < height; ++row) {
+
+        MPI_Recv(data_msg, width+1, MPI_LONG, MPI_ANY_SOURCE,
+                DATA_TAG, MPI_COMM_WORLD, &status);
+
+        this_row = data_msg[0];
+
+        for (col = 0; col < width; ++col) {
+            //colors[this_row][col] = data_msg[col + 1];
+            colors[col][this_row] = data_msg[col + 1];
+           // printf("%d ", colors[this_row][col]);
+        }
+    }
+    
+    plot_pgm(file_out, width, height, colors);
+
 	return EXIT_SUCCESS;
 }
 
-int slave(int rank, int num_tasks, Input in)
+int slave(int myID, int width, int height, Input in)
 {
-	int width, height, this_row, row, col;
-	int **colors;
-	MPI_Status status;
-	int *data_msg;
-	complex_t z, c;
-	double u, v;
-	
-	width = floor((in.x_max - in.x_min) / in.rezolutie);
-	height = floor((in.y_max - in.y_min) / in.rezolutie);
-	
-	data_msg = malloc((width + 2) * sizeof(int));
-	
-	colors = malloc(height * sizeof(int*));
-	for(row = 0; row < height; ++row)
-		colors[row] = malloc(width * sizeof(int));
-	int step = 0;
-	for(row = (rank - 1); row < height; row = row + num_tasks) {
-		data_msg[0] = row;
-		
-		for(col = 0; col < width; ++col) {
+	long start_msg[START_LNG];
+    MPI_Status status;
+    int first_row, rows;
+    long *data_msg;
+    data_msg = malloc((width + 1) * sizeof(long));
+    
+    /* Receive "start" message and extract data */
+    MPI_Recv(start_msg, START_LNG, MPI_LONG, 0, START_TAG,
+            MPI_COMM_WORLD, &status);
+    first_row = start_msg[0];
+    rows = start_msg[1];
+    
+    int row, col, step = 0;
+    complex_t z,c;
+    double u,v;
+    for(row = first_row; row < (first_row + rows); ++row) {
+    	data_msg[0] = row;
+    	for(col = 0; col < width; ++col) {
 			z = new_zero_complex();
 			step = 0;
-			u = in.x_min + row * in.rezolutie;
-			v = in.y_min + col * in.rezolutie;
+			u = in.x_min + col * in.rezolutie;
+			v = in.y_min + row * in.rezolutie;
 			c = new_complex(u,v);
 			while(modul(z) < 2 && step < in.max_steps) {			
 				z = add(mult(z, z), c);
 				step++;
 			}
-			data_msg[col + 1] = step % NUM_COLORS; 
-		}
-		 MPI_Send(data_msg, width+1, MPI_INT, 0, 1,
-                MPI_COMM_WORLD);
-	}
-		
+			data_msg[col + 1] = step % NUM_COLORS;
+    	}
+    	MPI_Send(data_msg, width+1, MPI_LONG, 0, DATA_TAG, MPI_COMM_WORLD);
+    }
+    
 	return EXIT_SUCCESS;
 }
 
@@ -86,21 +108,19 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	
-	int num_tasks, rank, len, rc;
+	int nprocs, myid, len, rc = 0;
 	char hostname[MPI_MAX_PROCESSOR_NAME];
 	
 	rc = MPI_Init(&argc,&argv);
 	if (rc != MPI_SUCCESS) {
- 	printf ("ERROR: could not start MPI program. QUIT.\n");
+ 		printf ("ERROR: could not start MPI program. QUIT.\n");
 		MPI_Abort(MPI_COMM_WORLD, rc);
+		exit(EXIT_FAILURE);
 	}
 	
-	MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	MPI_Get_processor_name(hostname, &len);
-	
-	//fprintf(stderr, "Number of tasks = %d \n My rank= %d \n Running on %s\n",
-//			num_tasks,rank,hostname);
 	
 	Input in;
 	in = parse_input_file(argv[1]);
@@ -109,84 +129,14 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	
-	
 	int width = floor((in.x_max - in.x_min) / in.rezolutie);
 	int height = floor((in.y_max - in.y_min) / in.rezolutie);
-	double u, v;
-	complex_t z, c;
-	int i, j, step = 0, **colors;
-		
-	colors = malloc(height * sizeof(int*));
-	for(j = 0; j < height; ++j)
-		colors[j] = malloc(width * sizeof(int));
-
-	switch (in.tip_multime) {
-		case 0:
-			z = new_zero_complex();
-			i = j = step = 0;
-			
-			// for(u = in.x_min ; u < in.x_max; u = u + in.rezolutie){
-			//	for(v = in.y_min ; v < in.y_max; v = v + in.rezolutie) { 
-			for(j = 0; j < height; ++j) {
-				for(i = 0; i < width; ++i) {	
-					z = new_zero_complex();
-					step = 0;
-					u = in.x_min + i * in.rezolutie;
-					v = in.y_min + j * in.rezolutie;
-					c = new_complex(u,v);
-					while(modul(z) < 2 && step < in.max_steps) {			
-						z = add(mult(z, z), c);
-						step++;
-					}
-					colors[i][j] = step % NUM_COLORS;
-//					colors[i / width][i % width] = step % NUM_COLORS;
-//					lastCounted = i;				
-//					++i;
-//					if(lastCounted / width != i / width || i / width == width) {
-//						break;
-//					}
-				}
-//					if(i / width == width){
-//						break;
-//					}
-			}
-			plot_pgm(argv[2], width, height, colors);
-			break;
-		case 1:
-			z = new_zero_complex();
-			c = new_complex(in.julia_param1, in.julia_param2);
-			i = j = step = 0;
-//			for(u = in.x_min; u < in.x_max; u = u + in.rezolutie){
-//				j = 0;			
-//				for(v = in.y_min ; v < in.y_max; v = v + in.rezolutie) {
-			for(j = 0; j < height; ++j) {
-				for(i = 0; i < width; ++i) {
-						u = in.x_min + i * in.rezolutie;
-						v = in.y_min + j * in.rezolutie;
-						z = new_complex(u, v);
-						step = 0;
-						while( modul(z) < 2 && step < in.max_steps) {
-							z = add(mult(z, z), c);
-							step++;
-						}
-						colors[i][j] = step % NUM_COLORS;
-//						colors[i / width][j] = step % NUM_COLORS;
-//						lastCounted = i;				
-//						++i; ++j;
-//						if(j == height){
-//							break;
-//						}
-					}
-//					if(i / width == width){
-//						break;
-//					}	
-				}
-			plot_pgm(argv[2], width, height, colors);
-			break;
-		
-		default:
-			printf("ERROR: Set not implemented. Accepting only Mandelbrot (0) or Julia (1) \v");
-			exit(EXIT_FAILURE);
+	
+	if(myid == 0) {
+		rc = master(nprocs - 1, width, height, in, argv[2]);
+		printf("Cred ca a mers \n");
+	} else {
+		rc = slave(myid, width, height, in);
 	}
 	
 	MPI_Finalize();
--- util.h
+++ util.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #define NUM_COLORS 256
 
 typedef struct _input {
